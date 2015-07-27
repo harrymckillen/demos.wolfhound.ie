2189fb1352ae08fd572ba2bc2c13e76ed4dbdab1
Harry McKillen <harry@wolfhound.ie>
2015-07-27 22:01:26 +0100

    First commit of some demos

---
+++ Gruntfile.js
@@ -0,0 +1,104 @@
+module.exports = function (grunt) {
+  grunt.log.writeln('Testing');
+  grunt.log.oklns('Testing');
+  grunt.log.errorlns('Testing');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      build: {
+        files: [
+          // { src: 'src/*.html', dest: 'build/*.html' },
+          {expand: true, cwd: 'src/', src: ['**/*.html'], dest: 'build/'},
+          {expand: true, cwd: 'src/', src: ['font/**'], dest: 'build/'},
+          {expand: true, cwd: 'src/', src: ['img/**'], dest: 'build/'},
+          {expand: true, cwd: 'src/', src: ['js/**'], dest: 'build/'}
+        ]
+      },
+    },
+    sass: {
+      options: {
+        style: 'compressed',
+        sourcemap: 'none'
+      },
+      build: {
+        files:  [
+          {
+            expand: true,
+            cwd: 'src/sass',
+            src: ['**/*.scss'],
+            dest: 'build/css',
+            ext: '.css'
+          }
+        ]
+        /*{
+          'build/css/*.css': 'src/sass/*.scss',
+          'build/css/demos/*.css': 'src/sass/demos/*.scss'
+        }*/
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          hostname: 'localhost',
+          port: 4000,
+          base: 'build/',
+          open: true
+        }
+      }
+    },
+    clean: {
+      build: {
+        src: ['build/']
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass:build'],
+        options: {
+          livereload: true,
+        },
+      },
+    },
+    processhtml: {
+      options: {
+        data: {
+          sitetitle: 'Wolfhound Media Design',
+          message: 'Hello world!'
+        }
+      },
+      build: {
+        files: [ 
+          {expand: true, cwd: 'build/', src: ['*.html'], dest: 'build/'} 
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.registerTask('build',
+    [
+      'clean:build',
+      'sass:build',
+      'copy:build',
+      'processhtml:build'
+    ]);
+  grunt.registerTask('default',
+    [
+      'build'
+    ]);
+  grunt.registerTask('serve',
+    [
+      'build',
+      'watch',
+      'connect'
+    ]);
+};
